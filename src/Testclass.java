712140b89c8758d20c13a7e9059bdd90c089c9f2
Matthias Bultmann <mbultmann@uos.de>
2016-05-30 21:23:37 +0200

    Update Testclass.java

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,7 +1,7 @@
 public class Testclass{
 
 public static void main(String[] args){
-	System.out.println("Hallo Welt. Wie schön, dass es dich gibt.");
+	System.out.println("Hallo Welt. Hier spricht Matthias. Wie schÃ¶n, dass es dich gibt.");
 }
 
 }
