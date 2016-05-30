ce072967f8b1a4af3169ae0fa92100073d84ff8e
Matthias <mbultmann@uos.de>
2016-05-30 21:20:00 +0200

    Neu

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,7 +1,7 @@
 public class Testclass{
 
 public static void main(String[] args){
-	System.out.println("Hallo Welt. Wie schön, dass es dich gibt.");
+	System.out.println("Hallo Welt. Wie schön, dass es dich gibt. Hallo Welt!");
 }
 
 }
