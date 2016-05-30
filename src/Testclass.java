34c518a68012a0465c2fcad045a0c7facbfb23d3
Matthias <mbultmann@uos.de>
2016-05-30 21:49:01 +0200

    print methonde

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,7 +1,11 @@
 public class Testclass {
 
+	public static void print(){
+		System.out.println("Hallo Welt");
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt.");
+		print();
 	}
 
 }
