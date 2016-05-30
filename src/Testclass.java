81f9fa2d9425386459d09ec13cb0bde8c34a483c
Matthias Bultmann <mbultmann@uos.de>
2016-05-30 22:00:01 +0200

    Update Testclass.java

--- src/Testclass.java
+++ src/Testclass.java
@@ -1,7 +1,11 @@
 public class Testclass {
 
+	public static void ausgabe(){
+		System.out.println("Ausgabe");
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Hallo Welt");
+		ausgabe();
 	}
 
 }
